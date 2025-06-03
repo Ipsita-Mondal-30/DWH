86c7cf6101699446946679fac7fc5351554f56dd
Ipsita <ipsitaamondal@gmail.com>
2025-06-03 22:46:11 +0530

    Restored and fixed next.config.js after merge conflict

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 // next.config.js
 module.exports = {
   images: {
-<<<<<<< HEAD
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'some-other-domain.com'],
-=======
-    domains: ['res.cloudinary.com'],
->>>>>>> f527b2746b3f9c6d31b94cca8e40e4645248674c
+    domains: [
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'some-other-domain.com',
+      'res.cloudinary.com',
+    ],
   },
 };
