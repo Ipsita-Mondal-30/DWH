847652fa431424160b286f5c114ce9b1adbbd2cc
Avi Srivastava <srivastavaavi26@gmail.com>
2025-06-06 21:04:04 +0530

    feat: Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,20 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import QueryProvider from '@/providers/QueryProvider';
 import ClientProviders from "./ClientProviders";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
         <QueryProvider >
-          <ClientProviders>{children}</ClientProviders>
+          <ClientProviders>
+            {children}
+           
+            </ClientProviders>
+
         </QueryProvider>
+        <Analytics />
       </body>
     </html>
   );
