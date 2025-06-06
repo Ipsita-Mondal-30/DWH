a831e5c1be8e813f51eaab1754420ea8f8ea47d7
Avi Srivastava <srivastavaavi26@gmail.com>
2025-06-06 21:16:32 +0530

    merge code

--- src/app/[product]/page.tsx
+++ src/app/[product]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div className='text-primary '>This shows the info about particular product (sweet)
-    <div className="text-accent">avi gadha</div>
+    {/* <div className="text-accent">avi gadha</div> */}
     </div>
   )
 }
